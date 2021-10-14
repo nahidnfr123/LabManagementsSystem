4a8b687b80caa947562799ccbf8414dc5b3ce1a5
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2021-10-14 22:08:41 +0600

    appointment table

---
+++ database/migrations/2021_10_14_155936_create_appointments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('appointment_no');//auto generated number
+            $table->dateTime('appointment_date');
+            $table->double('cost', 10, 2)->nullable();
+            $table->foreignId('users_id')->constrained(); //FK
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
